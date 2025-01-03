40df59f40173203794a3000c992cae15be2598fd
Seatwoom <pashchenko.alr@gmail.com>
2025-01-03 18:02:44 +0200

    fix: update eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,22 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import js from '@eslint/js';
 
-
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      ecmaVersion: 2022,
+      sourceType: 'module',
+    },
+    plugins: [],
+    extends: [],
+    rules: {
+      semi: 'error',
+      'no-unused-vars': 'warn',
+    },
+  },
 ];
